06394a344e12fc28db3112f6943327e37f1cc409
Paul Thompson <paul@findingfocus.dev>
2026-01-17 16:19:42 -0700

    sveltekit start with background

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+</script>
+
+<div class="relative w-full h-screen">
+    <img src="volcano.webp" alt="Volcano" class="w-full h-full object-cover"/>
+    <div class="absolute inset-0 bg-gradient-to-t from-purple-950/90 to-purple-950/75 pointer-events-none"></div>
+</div>
